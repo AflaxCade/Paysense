ba0cb9dc40e36702d3e70912aa0cd7eb9f1aaefe
AflaxCade <aflah.cade@gmail.com>
2023-12-03 17:32:23 +0300

    Update models.py

--- app/models.py
+++ app/models.py
@@ -4,7 +4,6 @@ from flask_login import UserMixin
 from itsdangerous import URLSafeTimedSerializer as Serializer
 
 
-
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
